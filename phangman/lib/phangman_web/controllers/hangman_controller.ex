3ba2992a2283e53b07309e57eb811bb3993af14b
bghalami <benjaminghalami@gmail.com>
2019-07-17 16:38:36 -0600

    Created controller and views for handling new game

--- phangman/lib/phangman_web/controllers/hangman_controller.ex
+++ phangman/lib/phangman_web/controllers/hangman_controller.ex
@@ -2,13 +2,19 @@ defmodule PhangmanWeb.HangmanController do
   use PhangmanWeb, :controller
 
   def new_game(conn, _params) do
-    render(conn, "new_game.html")
+    render(conn, "new_game.html", conn: conn)
   end
 
   def create_game(conn, _params) do
     game = Hangman.new_game()
+    tally = Hangman.tally(game)
+
     conn
     |> put_session( :game, game )
-    |> render( "game_field.html", game: game )
+    |> render( "game_field.html", tally: tally )
+  end
+
+  def word_so_far(tally) do
+    tally.guessed |> Enum.join(", ")
   end
 end
