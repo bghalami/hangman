80736787b0380f4133bfa555bd869e8afbf1b462
bghalami <benjaminghalami@gmail.com>
2019-07-19 14:49:21 -0600

    Created working websocket with phoenix

--- socket_phangman/lib/socket_phangman_web/channels/hangman_channel.ex
+++ socket_phangman/lib/socket_phangman_web/channels/hangman_channel.ex
@@ -1,8 +1,29 @@
 defmodule SocketPhangmanWeb.HangmanChannel do
 
+  require Logger
+
   use Phoenix.Channel
 
+  defimpl Jason.Encoder, for: [MapSet] do
+    def encode(struct, opts) do
+      Jason.Encode.list(Enum.to_list(struct), opts)
+    end
+  end
+
   def join("hangman:game", _, socket) do
+    game   = Hangman.new_game()
+    socket = assign(socket, :game, game)
+    Logger.error("You dun goofed")
     { :ok, socket }
+  end
+  def join("hangman:" <> _nothing, _, _socket) do
+    { :error, Logger.error("You dun goofed") }
+  end
+
+  def handle_in("tally", _, socket) do
+    game = socket.assigns.game
+    tally = Hangman.tally(game)
+    push(socket, "tally", tally)
+    { :noreply, socket }
   end
 end
--- socket_phangman/mix.exs
+++ socket_phangman/mix.exs
@@ -32,13 +32,14 @@ defmodule SocketPhangman.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:hangman, [path: "../hangman"]},
       {:phoenix, "~> 1.4.9"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
     ]
   end
 end
